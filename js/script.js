963f2ac766360ffe42a45cec34ca72c92e49401b
Joanna Pietruszka <joanna.pietuszka.94@gmail.com>
2021-05-15 14:27:54 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+{
+
+  const calculateResult = (amount, currency) => {
+    const rateEUR = 4.5562;
+    const rateUSD = 3.7579;
+
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
+
+      case "USD":
+        return amount / rateUSD;
+
+    }
+  };
+
+  const updateResultText = (amount, result, currency) => {
+    const resultElement = document.querySelector(".js-result");
+
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    updateResultText(amount, result, currency);
+
+  };
+  
+const init = () => {
+    const formElement = document.querySelector(".js-form");
+  
+    formElement.addEventListener("submit", onFormSubmit);
+    
+}
+
+  init();
+}
